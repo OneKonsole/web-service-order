b2cc4b8f65cb66a19110a302e969961d65841d3e
rlecomte19 <romanlecomte@gmail.com>
2024-01-24 15:35:57 +0100

    [FEAT] Adding management for paypal order information retrieval

--- app.go
+++ app.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"database/sql"
 	"fmt"
+	"io"
 	"log"
 	"os"
 
@@ -12,6 +13,8 @@ import (
 	"strconv"
 
 	oko "github.com/OneKonsole/order-model"
+	"github.com/OneKonsole/web-service-billing/helpers"
+	"github.com/OneKonsole/web-service-billing/paypal"
 
 	"github.com/gorilla/mux"
 
@@ -28,12 +31,14 @@ type App struct {
 }
 
 type AppConf struct {
-	ServedPort    string `json:"served_port"`     // e.g. "8010"
-	DBUser        string `json:"db_user"`         // e.g. "MyUsername"
-	DBPassword    string `json:"db_password"`     // e.g. "MyPassword1!"
-	DBDestination string `json:"db_URL"`          // e.g. "localhost" || "myservice.mynamespace.svc.cluster.local" || "onekonsole.fr"
-	DBName        string `json:"db_name"`         // e.g. "order"
-	SysServiceUrl string `json:"sys_service_url"` // e.g. "http://localhost:8020/sys-service/"
+	ServedPort         string `json:"served_port"`     // e.g. "8010"
+	DBUser             string `json:"db_user"`         // e.g. "MyUsername"
+	DBPassword         string `json:"db_password"`     // e.g. "MyPassword1!"
+	DBDestination      string `json:"db_URL"`          // e.g. "localhost" || "myservice.mynamespace.svc.cluster.local" || "onekonsole.fr"
+	DBName             string `json:"db_name"`         // e.g. "order"
+	SysServiceUrl      string `json:"sys_service_url"` // e.g. "http://localhost:8020/sys-service/"
+	PaypalClientID     string `json:"paypal_client_id"`
+	PaypalClientSecret string `json:"paypal_client_secret"`
 }
 
 // ===========================================================================================================
@@ -66,7 +71,7 @@ func (a *App) Initialize() {
 		panic(err)
 	}
 
-	fmt.Printf("[INFO] Opened postgresql connection for database %s.\n", a.AppConf.DBName)
+	fmt.Printf("[INFO] Opened postgresql connection for database.\n")
 
 	a.Router = mux.NewRouter()
 
@@ -89,9 +94,10 @@ func (appConf *AppConf) Initialize() {
 	appConf.DBDestination = os.Getenv("db_URL")
 	appConf.DBName = os.Getenv("db_name")
 	appConf.SysServiceUrl = os.Getenv("sys_service_url")
+	appConf.PaypalClientID = os.Getenv("paypal_client_id")
+	appConf.PaypalClientSecret = os.Getenv("paypal_client_secret")
 
 	fmt.Printf("[INFO] ...... Initializing app configurations ......\n")
-
 }
 
 // ===========================================================================================================
@@ -176,8 +182,6 @@ func (a *App) getOrders(w http.ResponseWriter, r *http.Request) {
 	count, _ := strconv.Atoi(r.FormValue("count"))
 	start, _ := strconv.Atoi(r.FormValue("start"))
 
-	fmt.Printf("[INFO] Trying to get all orders. \n")
-
 	if count > 10 || count < 1 {
 		count = 10
 	}
@@ -185,13 +189,117 @@ func (a *App) getOrders(w http.ResponseWriter, r *http.Request) {
 		start = 0
 	}
 
-	orders, err := oko.GetOrders(a.DB, start, count)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+	var bodyMap map[string]string
+
+	decoder := json.NewDecoder(r.Body)
+
+	err := decoder.Decode(&bodyMap)
+
+	if err != nil && err != io.EOF {
+		helpers.RespondWithError(w, http.StatusInternalServerError, "Could not decode user id in request body")
 		return
 	}
 
-	respondWithJSON(w, http.StatusOK, orders)
+	userID := bodyMap["user_id"]
+
+	if len(userID) > 0 {
+		fmt.Printf("[INFO] Asking all orders for user %s\n", bodyMap["user_id"])
+		orders, err := oko.GetOrders(a.DB, start, count, bodyMap["user_id"])
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		var orderIDs []string
+
+		for _, order := range orders {
+			orderIDs = append(orderIDs, order.PaypalID)
+		}
+
+		paypalOrders, err := GetOrderDetails(orderIDs)
+
+		if err != nil {
+			helpers.RespondWithError(w, http.StatusInternalServerError, "Could not retrieve paypal order details")
+			return
+		}
+
+		var returnedOrders []oko.OrderFullInfos
+
+		if len(orders) == len(paypalOrders) {
+			for i := 0; i < len(orders); i++ {
+				var currentFullOrder oko.OrderFullInfos
+				if orders[i].PaypalID == paypalOrders[i].ID {
+					currentFullOrder.AppOrder = orders[i]
+					currentFullOrder.PaypalOrder = paypalOrders[i]
+				}
+				returnedOrders = append(returnedOrders, currentFullOrder)
+			}
+			if len(returnedOrders) != len(orders) {
+				respondWithError(w, http.StatusInternalServerError, err.Error())
+				return
+			}
+		} else {
+			fmt.Printf("[ERROR] Number of retrieved Paypal orders isn't consistent with internal number of orders. \n")
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		fmt.Printf("[INFO] Retrieved paypal orders details\n")
+
+		respondWithJSON(w, http.StatusOK, returnedOrders)
+	} else {
+		fmt.Printf("[INFO] Asking all orders \n")
+		orders, err := oko.GetOrders(a.DB, start, count)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+		respondWithJSON(w, http.StatusOK, orders)
+	}
+}
+
+func GetOrderDetails(orderIds []string) ([]oko.PaypalOrderDetails, error) {
+	var ordersDetails []oko.PaypalOrderDetails
+
+	accessToken, err := paypal.GetAccessToken(
+		a.AppConf.PaypalClientID,
+		a.AppConf.PaypalClientSecret,
+	)
+	if err != nil {
+		fmt.Printf("[ERROR] Could not get paypal access token\n")
+		return nil, err
+	}
+	for _, orderId := range orderIds {
+		url := fmt.Sprintf("https://api-m.sandbox.paypal.com/v2/checkout/orders/%s", orderId)
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			fmt.Printf("[ERROR] Could not initiate request to get paypal order details\n")
+			return nil, err
+		}
+
+		fmt.Printf("[DEBUG] Retrieved access token : %s\n", accessToken)
+
+		req.Header.Add("Authorization", "Bearer "+accessToken)
+
+		client := &http.Client{}
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Printf("[ERROR] Could not make request to get paypal order details\n")
+			return nil, err
+		}
+		defer res.Body.Close()
+
+		var orderDetails oko.PaypalOrderDetails
+		if err := json.NewDecoder(res.Body).Decode(&orderDetails); err != nil {
+			fmt.Printf("[ERROR] Could not decode paypal response to get orders details \n %s", err)
+			return nil, err
+		}
+		fmt.Printf("[INFO] Decoded paypal order \n")
+
+		ordersDetails = append(ordersDetails, orderDetails)
+	}
+
+	return ordersDetails, nil
 }
 
 // ===========================================================================================================
@@ -214,18 +322,21 @@ func (a *App) getOrders(w http.ResponseWriter, r *http.Request) {
 // ===========================================================================================================
 func (a *App) createOrder(w http.ResponseWriter, r *http.Request) {
 
+	fmt.Printf("[INFO] Received request to create an order\n")
 	var o oko.Order
 	decoder := json.NewDecoder(r.Body)
 
 	if err := decoder.Decode(&o); err != nil {
-		respondWithError(w, http.StatusBadRequest, "[ERROR] Invalid request payload\n")
+		errMessage := "[ERROR] Invalid request payload decoding order to create\n"
+		fmt.Printf("%s", errMessage)
+		respondWithError(w, http.StatusBadRequest, errMessage)
 		return
 	}
 	defer r.Body.Close()
 
 	if err := a.Validator.Struct(o); err != nil {
-		errMessage := "[ERROR] One or more parameters do not match the required format. \n"
-		fmt.Printf("%s", errMessage)
+		errMessage := "One or more parameters do not match the required format."
+		fmt.Printf("[ERROR] %s\n", errMessage)
 		respondWithError(w, http.StatusBadRequest, errMessage)
 		return
 	}
@@ -251,6 +362,7 @@ func (a *App) createOrder(w http.ResponseWriter, r *http.Request) {
 	buf := new(bytes.Buffer)
 	json.NewEncoder(buf).Encode(o)
 
+	fmt.Printf("[INFO] Contacting sys order with order %d for user %s.\n", o.ID, o.UserID)
 	// Contact sys-order service
 	resp, err := http.Post(a.AppConf.SysServiceUrl, "application/json", buf)
 	if err != nil {
@@ -259,6 +371,8 @@ func (a *App) createOrder(w http.ResponseWriter, r *http.Request) {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
+	fmt.Printf("[INFO] Correctly created order %d for user %s.\n", o.ID, o.UserID)
+
 	defer resp.Body.Close()
 
 	respondWithJSON(w, http.StatusCreated, o)
